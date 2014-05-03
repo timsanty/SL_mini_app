07e6a4ddfdd322a53e09cbb7217396f4a61f59e1
timsanty <timsanty@me.com>
2014-05-03 09:53:34 -0700

    homepage working

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ StreetLampApp::Application.routes.draw do
 
 
 
-  root 'user#new'
+  root 'users#new'
 
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
