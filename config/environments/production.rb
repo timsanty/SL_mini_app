64a59651a4626f6e902db0499b15471b29300606
timsanty <timsanty@me.com>
2014-05-03 09:22:57 -0700

    adding left over file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,8 @@ StreetLampApp::Application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+  config.force_ssl = true
+
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
 
